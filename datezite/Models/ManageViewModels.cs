70db8969046b589a52255ae9de7dd27b64c10a1e
hampan <h.torpman@gmail.com>
2019-01-14 19:09:19 +0100

    yeaas

--- datezite/Models/ManageViewModels.cs
+++ datezite/Models/ManageViewModels.cs
@@ -109,5 +109,34 @@ namespace datezite.Models
         public List<ApplicationUser> Results { get; set; }
     }
 
+    public class PotentialMatchesViewModel
+    {
+        public List<ApplicationUser> Requests { get; set; }
+    }
+    public class MyMatchesViewModel
+    {
+        public List<ApplicationUser> Requests { get; set; }
+
+    }
+
+    public class EditYourProfileViewModel
+    {
+        public List<Interests> Interests { get; set; }
+        public List<ApplicationUser> Users { get; set; }
+        public ApplicationUser CurrentUser { get; set; }
+        public List<ApplicationUser> Requests { get; set; }
+        public string Användarnamn { get; set; }
+        public string Kön { get; set; }
+        public string Förnamn { get; set; }
+        public string Efternamn { get; set; }
+        public int Ålder { get; set; }
+        public string Lösenord { get; set; }
+        public string Sysselsättning { get; set; }
+        [Display(Name = "Profilbild")]
+        public byte[] UserPhoto { get; set; }
+
+
+    }
+
 
 }
